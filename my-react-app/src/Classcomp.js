619650c13dda1488e2cca81246209c844e5780ff
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-03 23:22:59 +0530

    assignment6

---
+++ my-react-app/src/Classcomp.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+const heading="Cognitive Clouds React Training"
+class Example extends Component{
+    
+    render()
+    {
+        return (<div>
+            <h1>{heading}</h1>
+        </div>)
+    }
+}
+  
+export default Example;
