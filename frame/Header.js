8642daf1f15c7288f29a1cc6144d1fd81645d023
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-09 21:39:13 +0530

    Delete Header.js

--- frame/Header.js
+++
@@ -1,9 +0,0 @@
-import styles from './Layout.module.css';    
-function Header(props) {
-    return (
-            <h3 className={styles.header}>HTML LAYOUT</h3>
-        
-    );
-}
-
-export default Header;
