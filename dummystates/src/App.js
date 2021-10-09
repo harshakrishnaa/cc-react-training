b16deacf604a0bb8028d3313bdefbeea1a4741bd
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-09 23:45:54 +0530

    assignment7

---
+++ dummystates/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Input from './Input';
+
+function App() {
+  return (
+    <div>
+      <h1>Data Pack Details</h1>
+      <Input></Input>
+    </div>
+  );
+}
+
+export default App;
