54bd9be8ff766dec5c46e878864476622b839706
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-12 23:05:10 +0530

    assignment8

---
+++ search-app/Stories.js
@@ -0,0 +1,31 @@
+import styles from './Search.module.css';
+function Stories({stories}) {
+    return (
+        <div>
+            <ol>
+            {stories.map(function(items, index)
+            {
+                return(
+                    <>
+                    <li key={index}>
+                        <div className={styles.span}>
+                        <span>
+                            <a href={items.url}>{items.title}</a>
+                        </span>
+                        <span>Author:{items.author}</span>
+                        <span>Comment:{items.num_Comment}</span>
+                        <span>Point:{items.point}</span>
+                        <span>Id:{items.object_id}</span>
+                        </div>
+                    </li>
+                    <hr/>
+                    </>
+                )
+            }
+            )
+            }</ol>
+        </div>
+    );
+}
+
+export default Stories;
