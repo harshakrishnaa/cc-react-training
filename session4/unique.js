366f9d9809b8e0667cbb9d631962a06af8e9212e
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-19 19:45:45 +0530

    Update unique.js

--- session4/unique.js
+++ session4/unique.js
@@ -1,12 +1,5 @@
-function unique(arr){
-    var newArr = [];
-    for(var i=0; i<arr.length; i++) {
-        if(newArr.indexOf(arr[i]) === -1) {
-            newArr.push(arr[i]);
-        }
-    }
-    return newArr;
-}
 let values=["green","green","red","red","yellow","pink","pink","yellow"];
-var uniquecolors=unique(values);
-console.log("Unique values are",uniquecolors);
+function  unique(arr){
+    return [...new Set(arr)];
+}
+unique(values);
