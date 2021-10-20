c6802916c2acf2a11c05da142a18e30bcad72e57
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-20 09:51:33 +0530

    Update destruct.js

--- session4/destruct.js
+++ session4/destruct.js
@@ -4,9 +4,8 @@ let usha = { name:"Usha",surname: "Janardhana",age:54};
 
 let users = [raksha,srinivas,usha]
 
-    const firstnameusers = users.map((value,index) => { return {
-            fullname: value.name + value.surname,
-            id:index+1
-        }
-    })
-    console.log(firstnameusers)
+users.map(user, index)=>{
+    return { 
+        fullname: '${user.name} ${user.surename} , id:index+1
+    }
+});
