2c27078740f49ce799ff0a918fc089bc2250ce05
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-20 08:03:20 +0530

    Update filtering.js

--- session4/filtering.js
+++ session4/filtering.js
@@ -1,2 +1,1 @@
-let returnLarger =(arr, num) => arr.filter(n => n >=num);
-console.log(returnLarger([1,33,99,50,100,150,60,99,1000,999,0,-1,10],150));
+[1,33,99,50,100,150,60,99,1000,999,0,-1,10]filter((item)=>item>=150);
