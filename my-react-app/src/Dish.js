52464f1334d96a4c6a8f80a4520e15fbe1459322
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-21 12:34:19 +0530

    Update Dish.js

--- my-react-app/src/Dish.js
+++ my-react-app/src/Dish.js
@@ -1,4 +1,35 @@
-// const groceryList=[ { "item": "Rice", "type": "veg" }, { "item": "Fish", "type": "non-veg" }, { "item": "Meat", "type": "non-veg" }, { "item": "Dal", "type": "veg" }]
+
+import React, {useState} from "react";
+const list=[ { "item": "Rice", "type": "veg" }, { "item": "Fish", "type": "non-veg" }, { "item": "Meat", "type": "non-veg" }, { "item": "Dal", "type": "veg" }]
+
+export default Dish;
+function filterOnType(arr, type){
+  return arr
+  .filter((item)=>item.type===type)
+  .map((item)=><li key={item.id}>{item.item}</li>);
+}
+const GroceryList =(props)=>{
+    const [sortedList, setSortedList]=useState{list};
+        function handleSort(){
+            const sortedList={..list.sort((a,b=>a.item.localCompare(b.item))];
+            setSortedList(sortedList);
+        }
+    return(
+        <div className={styles.container}>
+        <div className={styles.type}>
+            <h3>Veg Items</h3>
+            <ul>{filterOnType(list,"veg")}</ul>
+            <button onCLick={handleSort}>Sort</button>
+        </div>
+        <div className={styles.type}>
+            <h3>Non Veg Items</h3>
+            <ul>{filterOnType(list, "non-veg")}</ul>
+        </div>
+        </div>
+     );
+};
+export default GroceryList;
+// const List=[ { "item": "Rice", "type": "veg" }, { "item": "Fish", "type": "non-veg" }, { "item": "Meat", "type": "non-veg" }, { "item": "Dal", "type": "veg" }]
 // let NonVeg = groceryList.map((d) => {
 //     if(d.type==="non-veg")
 //     {
@@ -55,28 +86,4 @@
 //     </div>
   
 // );
-}
-
-
-export default Dish;
-const GroceryList =(props)=>{
-    const [sortedList, setSortedList]=useState{list};
-        function handleSort(){
-            const sortedList={..list.sort((a,b=>a.item.localCompare(b.item))];
-            setSortedList(sortedList);
-        }
-    return(
-        <div className={styles.container}>
-        <div className={styles.type}>
-            <h3>Veg Items</h3>
-            <ul>{filterOnType(list,"veg")}</ul>
-            <button onCLick={handleSort}>Sort</button>
-        </div>
-        <div className={styles.type}>
-            <h3>Non Veg Items</h3>
-            <ul>{filterOntype(list, "non-veg")}</ul>
-        </div>
-        </div>
-     );
-};
-export default GroceryList;
+// }
