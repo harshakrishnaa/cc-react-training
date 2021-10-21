739cb248de9d3fb48c7b24210e16b46f1aedb56c
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-21 11:46:08 +0530

    Update Dish.js

--- my-react-app/src/Dish.js
+++ my-react-app/src/Dish.js
@@ -1,61 +1,82 @@
-const groceryList=[ { "item": "Rice", "type": "veg" }, { "item": "Fish", "type": "non-veg" }, { "item": "Meat", "type": "non-veg" }, { "item": "Dal", "type": "veg" }]
-let NonVeg = groceryList.map((d) => {
-    if(d.type==="non-veg")
-    {
-        var nonveg=h.item;
-    }
-    return nonveg;
-    })
-    .filter((item)=>
-    {
-        return item!== undefined;
-    }
-    );
-let Veg = groceryList.map((d) => {
-    if(d.type==="veg")
-    {
-        var veg=d.item;
-    }
-    return veg;
-    })
-    .filter((item)=>
-    {
-        return item!== undefined;
-    }
-    );
+// const groceryList=[ { "item": "Rice", "type": "veg" }, { "item": "Fish", "type": "non-veg" }, { "item": "Meat", "type": "non-veg" }, { "item": "Dal", "type": "veg" }]
+// let NonVeg = groceryList.map((d) => {
+//     if(d.type==="non-veg")
+//     {
+//         var nonveg=h.item;
+//     }
+//     return nonveg;
+//     })
+//     .filter((item)=>
+//     {
+//         return item!== undefined;
+//     }
+//     );
+// let Veg = groceryList.map((d) => {
+//     if(d.type==="veg")
+//     {
+//         var veg=d.item;
+//     }
+//     return veg;
+//     })
+//     .filter((item)=>
+//     {
+//         return item!== undefined;
+//     }
+//     );
 
-let Sort=()=>
-{   
-Veg=Veg.sort();
-NonVeg=NonVeg.sort();
-alert(`Sorted:Veg= ${Veg} Non-Veg= ${NonVeg}`);
-}
-function Dish() {
-return (
-    <div className="main">
-        <table>
-            <thead>
-              <tr>
-                <th id="Veg">Veg</th>
-                <th id="Non-Veg">Non Veg</th>
-              </tr>
-            </thead>
-            <tbody id="menu">     
-                <tr>
-                  <td>{Veg[0]}</td>
-                  <td>{NonVeg[0]}</td>
-                </tr>
-              <tr>
-                <td>{Veg[1]}</td>
-                <td>{NonVeg[1]}</td>
-              </tr>
-            </tbody>
-        </table>
-        <button onClick={Sort}>Sort</button>
-    </div>
+// let Sort=()=>
+// {   
+// Veg=Veg.sort();
+// NonVeg=NonVeg.sort();
+// alert(`Sorted:Veg= ${Veg} Non-Veg= ${NonVeg}`);
+// }
+// function Dish() {
+// return (
+//     <div className="main">
+//         <table>
+//             <thead>
+//               <tr>
+//                 <th id="Veg">Veg</th>
+//                 <th id="Non-Veg">Non Veg</th>
+//               </tr>
+//             </thead>
+//             <tbody id="menu">     
+//                 <tr>
+//                   <td>{Veg[0]}</td>
+//                   <td>{NonVeg[0]}</td>
+//                 </tr>
+//               <tr>
+//                 <td>{Veg[1]}</td>
+//                 <td>{NonVeg[1]}</td>
+//               </tr>
+//             </tbody>
+//         </table>
+//         <button onClick={Sort}>Sort</button>
+//     </div>
   
-);
+// );
 }
 
 
 export default Dish;
+const GroceryList =(props)=>{
+    const [sortedList, setSortedList]=useState{list};
+        function handleSort(){
+            const sortedList={..list.sort((a,b=>a.item.localCompare(b.item))];
+            setSortedList(sortedList);
+        }
+    return(
+        <div className={styles.container}>
+        <div className={styles.type}>
+            <h3>Veg Items</h3>
+            <ul>{filterOnType(list,"veg")}</ul>
+            <button onCLick={handleSort}>Sort</button>
+        </div>
+        <div className={styles.type}>
+            <h3>Non Veg Items</h3>
+            <ul>{filterOntype(list, "non-veg")}</ul>
+        </div>
+        </div>
+     );
+};
+export default GroceryList;
