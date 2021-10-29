f66834319d35d695a67fb1b5d51ec1941776cded
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-29 23:03:26 +0530

    Update InputWithLabel.js

--- session14/components/InputWithLabel.js
+++ session14/components/InputWithLabel.js
@@ -1,38 +1,29 @@
-import React, { useEffect,useRef, useState } from "react";
+import React, {useEffect, useState, useRef} from "react";
 
-class InputWithlabel=({})
-    id, 
-    children,
-    type = "text",
-    value, 
-    onInputChange,
-    isFocused,
-    =>{
-        const [clickAutoFocus, setClickAutoFocus]= useState(false);
-        const inputRef =useRef();
-        const clickAutoFocus=true;
-        setClickAutoFocus(false);
+const InputWithlabel = ({id, children, type="text", value, onInputChange}) => {
     
-        useEffect(() => {
-            if(inputRef.current &&clickAutoFocus {
-                inputRef.current.focus();
-            },[clickAutoFocus]);
 
-        return (
-            <>
-                <label htmlFor={id}>{children}</label>
-                <input
-                ref={inputRef}
-                id = {id}  
-                type={type}
-                value={value} 
-                onChange={onInputChange}
-                />
-                <button onClick={() => setClickAutoFocus(true)}>
-                       Click to Autofocus
-                </button>
-            </>
-        );
-        };
+    
+    return (
+
+    <>
+        <label htmlFor={id}>{children}</label>
+        <input
+        style={{
+            display: "block",
+            padding: "1rem",
+            border: "none",
+            outline: "none",
+            borderRadius: "4px",
+            margin: "1rem 0",
+        }}
+         id = {id}  
+         type={type}
+          value={value} 
+          onChange={onInputChange}
+           />
+          </>
+);
+    }
 
 export default InputWithlabel;
