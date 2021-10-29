013dc252dd5cd14eeb18be06461d9e828945d166
harshakrishnaa <55657457+harshakrishnaa@users.noreply.github.com>
2021-10-29 23:04:45 +0530

    Update List.js

--- session14/components/List.js
+++ session14/components/List.js
@@ -1,28 +1,31 @@
+import styles from "./List.module.css";
 
-function List({stories}) {
+function List({stories, handleDeleteItem})   {
     return (
-        <div>
-            {stories.map(function(items)
-            {
-                return(
-                    <>
-                    <li>
-                        <span>
-                            <a href={items.url}>{items.title}</a>
-                        </span>
-                        <ul>
-                            <li>Author:{items.author}</li>
-                            <li>Comment:{items.num_Comment}</li>
-                            <li>Point:{items.point}</li>
-                            <li>Id:{items.object_id}</li>
-                        </ul>
-                    </li>
-                    </>
-                )
-            }
-            )}
-        </div>
-    );
-}
-
-export default List;
+      <div>
+        <ol>
+          {stories.map(function(item, index) {
+      return (
+        <li key = {index}>
+          <span className={styles.item}>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span className={styles.item}>{item.author}</span>
+          <span className={styles.item}>{item.num_comments}</span>
+          <span className={styles.item}>{item.points}</span>
+          <span>
+            <button 
+            className={styles.button} 
+            onClick={() => handleDeleteItem(item)}>
+              Delete
+              </button>
+          </span>
+          
+        </li>
+      );
+    })}
+    </ol>
+    </div>
+    )}
+  
+  export default List;
